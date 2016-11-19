26f72246a21136d220b707f69e96ca3a07280130
Adri Mtnez <ame97@correo.ugr.es>
2016-11-19 22:48:50 +0100

    Update principal.cpp

--- principal.cpp
+++ principal.cpp
@@ -57,39 +57,5 @@ int main(int argc, char *argv[]){
    //Imprimir número de elementos almacenados en conjuntoMutaciones
    cout << "Lectura del fichero finalizada. Mutaciones cargadas: " << conjuntoMutaciones.size() << endl;  
    
-   //Imprimir cuántas mutaciones están asociadas al cromosoma 1:
-   cout << "Mutaciones asociadas a Chr 1: " << *conjuntoMutaciones.lower_bound("2",1) << endl;
-   
-   //for(typename conjunto::iterator i = conjuntoMutaciones.begin() ; i < conjuntoMutaciones.lower_bound("2",1) ; i++)
-   //	cout << *i << endl;
-
-   /**@todo ¿Existe la mutación con ID "rs147165522"? Imprimir la mutación y las enfermedades asociadas */
-	typename conjunto::value_type mut;
-	bool esta;
-	
-	pair<typename conjunto::value_type, bool> (mut, esta) = conjuntoMutaciones.conjunto::find("rs147165522");
-	
-	if (esta){
-		cout << mut << endl;
-	}
-	else
-		cout << "No existe la mutacion con ID rs147165522" << endl;
-	
-   /**@todo ¿Existe la mutación en chr/pos "14"/67769578? Imprimir la mutación y las enfermedades asociadas */
-	pair<typename conjunto::value_type, bool> (mut, esta) = conjuntoMutaciones.conjunto::find("14",67769578);
-	
-	if (esta)
-		cout << mut << endl;
-	else
-		cout << "No existe la mutacion en la posicion 67769578 del cromosoma 14" << endl;
-	
-   /**@todo ¿Cómo podríamos calcular el número de mutaciones del cromosoma 3? (utiliza lower_bound / upper_bound) */
-	
-	//cout << "Hay " << conjuntoMutaciones.lower_bound("4",1) - conjuntoMutaciones.upper_bound("3",0) << " mutaciones en el cromosoma 3" << endl;
-	cout << "Lower -> " << *conjuntoMutaciones.lower_bound("4",1) << endl;
-	cout << "Upper -> " << *conjuntoMutaciones.upper_bound("3",0) << endl;
-	
-   /**@todo Analiza la eficiencia teórica y empírica de las operaciones find, insert y erase */
-   
    return 0;
 }
